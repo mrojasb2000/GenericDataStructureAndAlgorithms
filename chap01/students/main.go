0eab2127370c5f3df4c77ea31ea01c7d5b6c1918
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-17 22:05:14 -0300

    chore: added function by ID

--- chap01/students/main.go
+++ chap01/students/main.go
@@ -6,10 +6,20 @@ func addStudent(students []string, student string) []string {
 	return append(students, student)
 }
 
+func addStudentID(students []int, student int) []int {
+	return append(students, student)
+}
+
 func main() {
 	students := []string{}
 	result := addStudent(students, "Michael")
 	result = addStudent(result, "Jennifer")
 	result = append(result, "Elaine")
 	fmt.Println(result)
+
+	students1 := []int{}
+	result1 := addStudentID(students1, 155)
+	result1 = addStudentID(result1, 112)
+	result1 = addStudentID(result1, 120)
+	fmt.Println(result1)
 }
