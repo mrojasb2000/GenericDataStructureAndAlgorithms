c995e92a92b2f9052d65af7f2db338d20369ca56
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-17 22:14:33 -0300

    chore: added struct

--- chap01/students/main.go
+++ chap01/students/main.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
+type Student struct {
+	Name string
+	ID   int
+	age  float64
+}
+
 func addStudent(students []string, student string) []string {
 	return append(students, student)
 }
 
 func addStudentID(students []int, student int) []int {
+	return append(students, student)
+}
+
+func addStudentStruct(students []Student, student Student) []Student {
 	return append(students, student)
 }
 
@@ -22,4 +32,10 @@ func main() {
 	result1 = addStudentID(result1, 112)
 	result1 = addStudentID(result1, 120)
 	fmt.Println(result1)
+
+	student2 := []Student{}
+	result2 := addStudentStruct(student2, Student{"John", 213, 17.5})
+	result2 = addStudentStruct(student2, Student{"James", 111, 18.75})
+	result2 = addStudentStruct(student2, Student{"Marsha", 110, 16.25})
+	fmt.Println(result2)
 }
