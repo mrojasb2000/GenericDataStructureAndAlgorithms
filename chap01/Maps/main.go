3f57e028abedf55242357cf9c4cb064bce069b87
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-20 13:54:07 -0300

    chore: replace generic map implementation

--- chap01/Maps/main.go
+++ chap01/Maps/main.go
@@ -10,9 +10,17 @@ func MyMap(input []int, f func(int) int) []int {
 	return result
 }
 
+func GenericMap[T1, T2 any](input []T1, f func(T1) T2) []T2 {
+	result := make([]T2, len(input))
+	for index, value := range input {
+		result[index] = f(value)
+	}
+	return result
+}
+
 func main() {
 	slice := []int{1, 5, 2, 7, 4}
-	result := MyMap(slice, func(i int) int {
+	result := GenericMap(slice, func(i int) int {
 		return i * i
 	})
 	fmt.Println(result)
