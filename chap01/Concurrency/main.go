051e11c4482bdd20152ad98173442553c167561b
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-22 12:42:03 -0300

    feat: basic concurrency

---
+++ chap01/Concurrency/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func regularFunction() {
+	fmt.Println("Just entered regularFunction()")
+	time.Sleep(5 * time.Second)
+}
+
+func gorotineFunction() {
+	fmt.Println("Just entered goroutineFunction()")
+	time.Sleep(10 * time.Second)
+	fmt.Println("goroutineFunction finished its work")
+}
+
+func main() {
+	go gorotineFunction()
+	fmt.Println("In main one line below goroutineFunction()")
+	regularFunction()
+	fmt.Println("In main one line below regularFunction()")
+}
