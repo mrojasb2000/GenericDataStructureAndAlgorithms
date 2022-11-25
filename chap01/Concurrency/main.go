96657102975a109ecf29356731ef25bc65991ffd
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-25 16:54:25 -0300

    feat: added WaitGroup concurrency functions

--- chap01/Concurrency/main.go
+++ chap01/Concurrency/main.go
@@ -2,23 +2,45 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
-func regularFunction() {
-	fmt.Println("Just entered regularFunction()")
-	time.Sleep(10 * time.Second)
+var wg sync.WaitGroup
+
+func outputStrings() {
+	defer wg.Done()
+	strings := [5]string{"One", "Two", "Three", "Four", "Five"}
+	for i := 0; i < 5; i++ {
+		delay := 1 + rand.Intn(3)
+		time.Sleep(time.Duration(delay) * time.Second)
+		fmt.Println(strings[i])
+	}
 }
 
-func gorotineFunction() {
-	fmt.Println("Just entered goroutineFunction()")
-	time.Sleep(5 * time.Second)
-	fmt.Println("goroutineFunction finished its work")
+func outputInts() {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		delay := 1 + rand.Intn(3)
+		time.Sleep(time.Duration(delay) * time.Second)
+		fmt.Println(i)
+	}
+}
+
+func outputFloats() {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		delay := 1 + rand.Intn(3)
+		time.Sleep(time.Duration(delay) * time.Second)
+		fmt.Println(float64(i*i) + 0.5)
+	}
 }
 
 func main() {
-	go gorotineFunction()
-	fmt.Println("In main one line below goroutineFunction()")
-	regularFunction()
-	fmt.Println("In main one line below regularFunction()")
+	wg.Add(3)
+	go outputStrings()
+	go outputInts()
+	go outputFloats()
+	wg.Wait()
 }
